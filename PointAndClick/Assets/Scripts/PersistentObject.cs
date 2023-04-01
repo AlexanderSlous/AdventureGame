1fbbfe8319fe9a0567b4ede47be0ef5a9238cb43
Alexander Slous <AlexanderSlous@outlook.com>
2023-04-02 00:30:23 +0100

    Implemented basic object state saving

--- PointAndClick/Assets/Scripts/PersistentObject.cs
+++ PointAndClick/Assets/Scripts/PersistentObject.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PersistentObject : MonoBehaviour
 {
+    public string SceneName;
+
+    //public static PersistentObject Instance;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        //Instance = this;
+        //DontDestroyOnLoad(gameObject);
+
+        if(SceneManager.GetActiveScene().name != SceneName)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
